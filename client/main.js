c4269a52bd8f3fbf3e55d49cad34745025fe1835
Brian Lau <lauwangtatbrian@gmail.com>
2017-08-03 14:57:31 -0700

    add layout to main.js

--- client/main.js
+++ client/main.js
@@ -1,13 +1,18 @@
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import React from 'react';
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import renderRoutes from '../imports/start-up/client/routes/';
+import Layout from '../imports/ui/components/Layout/';
 import Comments from '../imports/ui/containers/Comments/';
+
 
 const App = () => (
   <MuiThemeProvider>
-    {renderRoutes()}
+    <Layout>
+      {renderRoutes()}
+    </Layout>
   </MuiThemeProvider>
 );
 
