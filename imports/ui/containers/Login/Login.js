c05fe71dc5ecd4ff948f6fca2e4cfc739d2cb630
heyliddle <eric.liddle@gmail.com>
2017-08-08 09:24:33 -0700

    updated header

--- imports/ui/containers/Login/Login.js
+++ imports/ui/containers/Login/Login.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+
+import './style.css';
+
+const style = {
+    height: 100,
+    width: 100,
+    margin: 20,
+    textAlign: 'center',
+    display: 'inline-block',
+};
 
 const Login = () => (
-    <h1> Login! </h1>
+    <div className="theOneAbovePaper">
+        <Paper  className="ffs" style={style} zDepth={1}>
+            <TextField
+                className="testname"
+                hintText="Hint Text"
+                floatingLabelText="Fixed Floating Label Text"
+                floatingLabelFixed={true}
+            />
+        </Paper>
+    </div>
 );
 
 export default Login;
