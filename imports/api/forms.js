c3cb23969bb56f44ac84dee56e7b01a6e657bf45
Brian Lau <lauwangtatbrian@gmail.com>
2017-08-09 10:48:48 -0700

    add react state to output mutiple input data on mongo

--- imports/api/forms.js
+++ imports/api/forms.js
@@ -26,17 +26,27 @@ export const Forms = new Mongo.Collection('forms');
 
 // Allow Client to do these things only to this collection...
 Meteor.methods({
-  // add todo
-  'forms.addProfileFormData' (data) {
+  // add profile form data to db
+  'forms.addProfileFormData' (des, input1) {
     // check(text, String)
     // if (!this.userId) {
     //   throw new Meteor.Error('todos.toggleComplete.not-authorized')
     //     'Your are not authorized to update todo for others'
     // }
     Forms.insert({
-      title: data,     
-      complete: false,
-      owner: this.userId
+        title: "What did you get stuck on?",     
+        // TODO: add data format
+        //   owner: this.userId
+        // createOn
+        description: des,
+        // classcohort
+        // form
+        input1: input1
+        // input2
+        // input3
+        // input4
+        // input5
+        // takeaway
     })
   },
 })
