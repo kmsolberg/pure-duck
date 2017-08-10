18674b6508206612a4696c5433f5ae6473dd6822
Brian Lau <lauwangtatbrian@gmail.com>
2017-08-10 14:08:24 -0700

    add redux-form and connect to redux-store

--- imports/ui/redux/combine-reducers.js
+++ imports/ui/redux/combine-reducers.js
@@ -1,5 +1,7 @@
-import { createStore, combineReducers } from 'redux';
+import { combineReducers } from 'redux';
+// import { FormRenderer } from './modules/feedBackForm'
 import { reducer as formReducer } from 'redux-form';
+
 
 export default combineReducers({
   form: formReducer
---
+++ imports/ui/redux/modules/feedBackForm.js
@@ -0,0 +1,28 @@
+// // Action Constants
+// const LOAD_FORMS = 'LOAD_FORMS';
+
+// // Action Creators
+// export function loadForms(feedBackForms) {
+//   return {
+//     type: LOAD_FORMS,
+//     payload: feedBackForms
+//   };
+// }
+
+
+// // Reducers
+// const initialState = {
+//   loading: true,
+// //   itemFilter: []
+// };
+
+// export function FormRenderer(state = initialState, action) {
+//   switch (action.type) {
+//     case LOAD_FORMS:
+//         return { loading: false, formsData: action.payload };
+//     default:
+//         return state;
+
+//   }
+// }
+
