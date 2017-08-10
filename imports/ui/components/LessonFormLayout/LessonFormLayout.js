136917be8b48f73f809769feded81b49bece893d
Kat Solberg <katsolberg@gmail.com>
2017-08-10 14:04:21 -0700

    add redux form to lesson form

--- imports/ui/components/LessonFormLayout/LessonFormLayout.js
+++ imports/ui/components/LessonFormLayout/LessonFormLayout.js
@@ -1,141 +1,128 @@
 import React from 'react';
-import { Paper, DropDownMenu, MenuItem, TextField, RadioButton, RadioButtonGroup, RaisedButton } from 'material-ui';
+import { Field, reduxForm } from 'redux-form'
+import {
+    Paper,
+    DropDownMenu,
+    MenuItem,
+    RadioButton,
+    RaisedButton
+} from 'material-ui';
+
+import {
+    DatePicker,
+    RadioButtonGroup,
+    TextField
+} from 'redux-form-material-ui';
 
 import './style.css';
 
+function disableWeekends(date) {
+    return date.getDay() === 0 || date.getDay() === 6;
+}
 
-const LessonFormLayout = ({handleChange, handleSubmit, state}) => (
-    <form name="profileForm" className='Form' onSubmit={() => handleSubmit(event)} >
-        <Paper >
-            <h1>{state.title}</h1>
-            <h3>Which day are you reviewing?</h3>
-            {/* <DropDownMenu value={1}>
-                <MenuItem value={moment()} primaryText="Today" />                       
-                <MenuItem value={moment().subtract(1, 'day')} primaryText={moment().subtract(1, 'day').from()} />
-                <MenuItem value={moment().subtract(2, 'day')} primaryText={moment().subtract(2, 'day').from()} />
-                <MenuItem value={moment().subtract(3, 'day')} primaryText={moment().subtract(3, 'day').from()} />            
-            </DropDownMenu>  */}
+const required = value => (value == null ? 'Required' : undefined);
+
+let LessonFormLayout = ({ handleChange, handleSubmit, pristine, submitting, reset }) => (
+    <Paper >
+        <form name="profileForm" className='Form' onSubmit={() => handleSubmit(event)} >
+            <h3>Lessons</h3>
             <div>
-                <h1>How well do you think you did?</h1>
-                <h3>( 1-poorly, 5-well )</h3>
-                <RadioButtonGroup 
-                    name="oneToFiveRadio" 
-                    onChange={(event) => handleChange(event)} 
-                    valueSelected={state.oneToFiveRadio} 
-                >
-                    <RadioButton
-                        value="1"
-                        label="1"
-                        className='radioButton'
-                    />
-                    <RadioButton
-                        value="2"
-                        label="2"
-                        className='radioButton'
-                    />
-                    <RadioButton
-                        value="3"
-                        label="3"
-                        className='radioButton'
-                    />
-                    <RadioButton
-                        value="4"
-                        label="4"
-                        className='radioButton'
-                    />
-                    <RadioButton
-                        value="5"
-                        label="5"
-                        className='radioButton'
-                    />
-                </RadioButtonGroup>
+                <Field
+                    name="lessonDate"
+                    component={DatePicker}
+                    format={null}
+                    hintText="Date of lesson"
+                    validate={required}
+                />
             </div>
             <div>
-                <h1>Do you think the lessons adequately prepared you for the lab?</h1>
-                <RadioButtonGroup 
-                    name="trueOrFalse"
-                    onChange={(event) => handleChange(event)} 
-                    valueSelected={state.trueOrFalse} 
-                >
-                    <RadioButton
-                        value="true"
-                        label="Yes"
-                        className='radioButton'
-                    />
-                    <RadioButton
-                        value="false"
-                        label="No"
-                        className= 'radioButton'
-                    />
-                </RadioButtonGroup>
+                <h3>How well do you think you did?</h3>
+                <h4>( 1-poorly, 5-well )</h4>
+                <Field name="oneToFiveRadio" component={RadioButtonGroup}>
+                    <RadioButton value="1" label="1" />
+                    <RadioButton value="2" label="2" />
+                    <RadioButton value="3" label="3" />
+                    <RadioButton value="4" label="4" />
+                    <RadioButton value="5" label="5" />
+                </Field>
             </div>
             <div>
-                <h1>What did you get stuck on?</h1>
-                <TextField
+                <h3>What did you find difficult about the lesson, and why?</h3>
+                <Field
                     name="input1"
-                    value={state.input1}
-                    onChange={(event) => handleChange(event)}
+                    component={TextField}
                     hintText="Type Here"
-                    floatingLabelText="You know..."
-                    fullWidth={true}
-                    rows={2}
+                    floatingLabelText="Got stuck?"
+                    multiLine
+                    rows={3}
                 />
             </div>
             <div>
-                <h1>Anything you'd like to do but couldn't?</h1>
-                <TextField
+                <h3>What was something you learned?</h3>
+                <Field
                     name="input2"
-                    value={state.input2}
-                    onChange={(event) => handleChange(event)}  
+                    component={TextField}
                     hintText="Type Here"
-                    multiLine={true}
-                    rows={2}
                     floatingLabelText="What do you really think..."
-                    fullWidth={true}
+                    multiLine
+                    rows={3}
                 />
             </div>
             <div>
-                <h1>Any additions/alterations for this Lesson?</h1>
-                <TextField
+                <h3>Do you think the lesson adequately prepared you for the lab?</h3>
+                <Field name="trueOrFalse" component={RadioButtonGroup}>
+                    <RadioButton value="true" label="Yes" />
+                    <RadioButton value="false" label="No" />
+                </Field>
+            </div>
+            <div>
+                <h3>Any questions you wanted to ask but couldn't?</h3>
+                <Field
+                    name="input4"
+                    component={TextField}
+                    hintText="Type Here"
+                    floatingLabelText="That one thing you still don't get"
+                    multiLine
+                    rows={3}
+                />
+            </div>
+            <div>
+                <h3>Any other comments about the day?</h3>
+                <Field
                     name="input3"
-                    value={state.input3}
-                    onChange={(event) => handleChange(event)}  
+                    component={TextField}
                     hintText="Type Here"
                     floatingLabelText="Be honest!"
-                    multiLine={true}
-                    rows={2}
-                    fullWidth={true}	  
+                    multiLine
+                    rows={3}
                 />
             </div>
             <div>
-                <h1>One question you wanted to as today but couldn't?</h1>
-                <TextField
-                    name="input4"
-                    value={state.input4}
-                    onChange={(event) => handleChange(event)}  
-                    hintText="Type Here"
-                    multiLine={true}
-                    rows={2}
-                    floatingLabelText="What do you really think..."
-                    fullWidth={true}	  
-                />
-            </div>
-            <div>
-                <h1>Rapid takeaway!</h1>
-                <TextField
-                    name="input5"
-                    value={state.input5}
-                    onChange={(event) => handleChange(event)}  
+                <h3>Your summary of the day!</h3>
+                <Field
+                    name="takeaway"
+                    component={TextField}
                     hintText="Type Here"
                     floatingLabelText="140 characters or less"
-                    multiLine={true}
-                    rows={2}
-                    fullWidth={true}	
+                    rows={1}
                 />
             </div>
-            <RaisedButton label="Submit" secondary={true} fullWidth={true} type="submit"/>
-        </Paper>
-    </form>
+            <div>
+                <button type="submit" disabled={submitting}>Submit</button>
+                <button
+                    type="button"
+                    disabled={pristine || submitting}
+                    onClick={reset}
+                >
+                    Clear
+                </button>
+            </div>
+        </form>
+    </Paper>
 )
+
+LessonFormLayout = reduxForm({
+    form: 'lessonForm'
+})(LessonFormLayout)
 
 export default LessonFormLayout;
--- imports/ui/components/ProjectFormLayout/ProjectFormLayout.js
+++ imports/ui/components/ProjectFormLayout/ProjectFormLayout.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
  
 import { Paper, DropDownMenu, MenuItem, TextField, RadioButton, RadioButtonGroup, RaisedButton } from 'material-ui';
 
