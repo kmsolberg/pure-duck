931ec9446a9af3a5e37262a5320bc6e400c947ff
heyliddle <eric.liddle@gmail.com>
2017-08-12 21:46:17 -0700

    fixed more issues with login page

---
+++ imports/api/users.js
@@ -0,0 +1,20 @@
+// StudentSchema = new SimpleSchema({
+//   email: {
+//     type: String,
+//     max: 100
+//   },
+//   password: {
+//     type: String,
+//     max: 50
+//   }
+// });
+
+// CustomerSchema = new SimpleSchema({
+//   billingAddress: {
+//     type: AddressSchema
+//   },
+//   shippingAddresses: {
+//     type: [AddressSchema],
+//     minCount: 1
+//   }
+// });
