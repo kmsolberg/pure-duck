76d5b77902a6266c40d08c7ca3882c58c2ce8daf
heyliddle <eric.liddle@gmail.com>
2017-08-14 14:09:39 -0700

    changed fields on signup container

--- imports/ui/containers/Signup/Signup.js
+++ imports/ui/containers/Signup/Signup.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import { Accounts } from 'meteor/accounts-base';
 import AccountsUIWrapper from '../../components/AccountsUIWrapper/index.js';
-import { Paper, RaisedButton } from 'material-ui';
-import { Field, reduxForm } from 'redux-form';
-import { TextField } from 'redux-form-material-ui';
+import {
+    Paper,
+    RadioButton,
+    RaisedButton,
+    DropDownMenu,
+    MenuItem,
+    SelectField
+} from 'material-ui';
+
+import {
+    Field,
+    formValueSelector,
+    reduxForm
+} from 'redux-form';
+
+import {
+    TextField,
+    RadioButtonGroup
+} from 'redux-form-material-ui';
 
 const style = {
     margin: 12,
 };
 
+const formSelect = ({
+    input,
+    label,
+    meta: { touched, error },
+    children,
+    ...custom
+}) =>
+    <SelectField
+        floatingLabelText={label}
+        errorText={touched && error}
+        {...input}
+        onChange={(event, index, value) => input.onChange(value)}
+        children={children}
+        {...custom}
+    />
+
 let Signup = ({ signUp }) => (
+
+
+
     <div className="loginformwrapper">
         <AccountsUIWrapper />
         <form
@@ -60,27 +95,30 @@ let Signup = ({ signUp }) => (
                 {/* consider changing these to dropdown */}
                 <div className="signupCohort">
                     <h4>Cohort:</h4>
-                    <Field
-                        name="cohort"
-                        component={TextField}
-                        hintText="Cohort"
-                        floatingLabelFixed={true} />
+                    <Field name="cohort" label="Select Your Cohort" component={formSelect} >
+                        <MenuItem value={1} primaryText="1" />
+                        <MenuItem value={2} primaryText="2" />
+                        <MenuItem value={3} primaryText="3" />
+                        <MenuItem value={4} primaryText="4" />
+                        <MenuItem value={5} primaryText="5" />
+                        <MenuItem value={6} primaryText="6" />
+                        <MenuItem value={7} primaryText="7" />
+                        <MenuItem value={8} primaryText="8" />
+                    </Field>
                 </div>
                 <div className="signupClass">
                     <h4>Class:</h4>
-                    <Field
-                        name="course"
-                        component={TextField}
-                        hintText="Class"
-                        floatingLabelFixed={true} />
+                    <Field name="class" label="Select Your Class" component={formSelect} >
+                        <MenuItem value={1} primaryText="WDP" />
+                        <MenuItem value={2} primaryText="ADP" />
+                    </Field>
                 </div>
                 <div className="singupRole">
                     <h4>Role:</h4>
-                    <Field
-                        name="role"
-                        component={TextField}
-                        hintText="Class"
-                        floatingLabelFixed={true} />
+                    <Field name="role" component={RadioButtonGroup}>
+                        <RadioButton value="true" label="Student" />
+                        <RadioButton value="false" label="Instructor" />
+                    </Field>
                 </div>
                 <RaisedButton
                     label="Submit"
@@ -89,11 +127,11 @@ let Signup = ({ signUp }) => (
                 />
             </Paper>
         </form>
-    </div>
+    </div >
 );
 
 Signup = reduxForm({
-form: 'signupform'
+    form: 'signupform'
 })(Signup)
 
 export default Signup;
